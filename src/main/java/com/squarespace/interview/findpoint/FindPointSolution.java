54263eef3faf943740226e0b5734586b480bbb22
Marios Sifalakis <msifalakis@truckpay.com>
2018-02-01 12:07:02 -0500

    question 2 answered

--- src/main/java/com/squarespace/interview/findpoint/FindPointSolution.java
+++ src/main/java/com/squarespace/interview/findpoint/FindPointSolution.java
@@ -15,10 +15,47 @@ public class FindPointSolution {
    * @param rootNode Top-level node of the view hierarchy.
    * @param toFind An x,y coordinate to be located.
    * @return An ordered rdered list of {@link Node#id}
+   * Marios Sifalakis - Modified it to return emtpy LinkedList if point is not contained
+   * and kept and returned the LinkedList to show the node path for the point if found.
    */
   @SuppressWarnings("UnusedParameters")
   public static List<String> findPathToNode(Node rootNode, Point toFind) {
     // @ToDo Implement this routine
-    return new LinkedList<>();
+    LinkedList<Node> nodeList = new LinkedList();
+    return getAllNodesForPoint(rootNode, toFind, nodeList);
+  }
+
+  static LinkedList getAllNodesForPoint(Node n, Point p, LinkedList<Node> list) {
+    list.add(n);
+    for (Node temp : n.getChildren()) {
+      if (temp.getChildren().size() > 0) {
+        if (isContained(temp, p)) {
+          return list;
+        } else {
+          getAllNodesForPoint(temp, p, list);
+        }
+      } else {
+        list = new LinkedList<>();
+        return list;
+      }
+    }
+    list = new LinkedList<>();
+    return list;
+  }
+
+  static boolean isContained(Node e, Point p){
+    Point topLeft = new Point(e.getLeft(), e.getTop());
+    Point topRight = new Point(e.getLeft()+e.getWidth(), e.getTop());
+    Point bottomLeft = new Point(e.getLeft(), e.getTop()+e.getHeight());
+    Point bottomRight = new Point(e.getLeft()+e.getWidth(), e.getTop()+e.getHeight());
+    if(topLeft.getX()<p.getX()
+            &&p.getX()<topRight.getX()
+            &&topLeft.getY()<p.getY()
+            &&bottomLeft.getY()>p.getY()){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
 }
